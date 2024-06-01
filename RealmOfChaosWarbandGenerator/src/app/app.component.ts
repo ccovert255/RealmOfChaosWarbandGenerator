c782ed40bcee68d16cf94d8eb6e114cbd96cc263
Chris Covert <chriscovert@hotmail.com>
2024-06-01 10:27:34 -0700

    v2

--- RealmOfChaosWarbandGenerator/src/app/app.component.ts
+++ RealmOfChaosWarbandGenerator/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { GeneratorComponent } from './generator/generator.component'
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, GeneratorComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Realm Of Chaos Warband Generator';
---
+++ RealmOfChaosWarbandGenerator/src/app/generator/generator.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDivider, MatDividerModule } from '@angular/material/divider';
+
+
+@Component({
+  selector: 'app-generator',
+  standalone: true,
+  imports: [MatIconModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatDivider, FormsModule, MatDividerModule],
+  templateUrl: './generator.component.html',
+  styleUrl: './generator.component.scss'
+})
+export class GeneratorComponent {
+
+}
