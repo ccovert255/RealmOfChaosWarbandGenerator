a9a4f4c66243971b055ec4ab7ba1c214bb3d0172
Chris Covert <chriscovert@hotmail.com>
2024-06-17 16:07:03 -0700

    theme still kinda messed up

--- RealmOfChaosWarbandGenerator/src/app/app.component.ts
+++ RealmOfChaosWarbandGenerator/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterOutlet } from '@angular/router';
 import { GeneratorComponent } from './generator/generator.component'
 import { WarbandsListComponent } from './warbands-list/warbands-list.component'
@@ -7,7 +8,7 @@ import { AlertService } from './alert.service';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, GeneratorComponent, WarbandsListComponent],
+  imports: [RouterOutlet, GeneratorComponent, WarbandsListComponent, MatIconModule],
   providers: [AlertService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
