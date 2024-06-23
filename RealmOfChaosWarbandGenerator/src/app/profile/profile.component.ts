63fd287606de0e75526da4b7f2526e3ea8666ad7
Chris Covert <chriscovert@hotmail.com>
2024-06-23 10:42:38 -0700

    profile now displays hero leadership bonuses that apply to unit

--- RealmOfChaosWarbandGenerator/src/app/profile/profile.component.ts
+++ RealmOfChaosWarbandGenerator/src/app/profile/profile.component.ts
@@ -11,4 +11,40 @@ import { Warband, Profile, Weapon, Armor, ChaosAttribute, Champion } from '../sh
 })
 export class ProfileComponent {
   @Input() champion?: Champion;
+
+  leadershipStatDisplay(): string {
+    if (this.champion?.characterBonus == null) return '';
+
+    if (this.champion.characterBonus.leadership > 0)
+      return `${this.champion.profileWithBonuses().leadership}+${this.champion.characterBonus.leadership}`;
+
+    return String(this.champion.profileWithBonuses().leadership);
+  }
+
+  intelligenceStatDisplay(): string {
+    if (this.champion?.characterBonus == null) return '';
+
+    if (this.champion.characterBonus.intelligence > 0)
+      return `${this.champion.profileWithBonuses().intelligence}+${this.champion.characterBonus.intelligence}`;
+
+    return String(this.champion.profileWithBonuses().intelligence);
+  }
+
+  coolStatDisplay(): string {
+    if (this.champion?.characterBonus == null) return '';
+
+    if (this.champion.characterBonus.cool > 0)
+      return `${this.champion.profileWithBonuses().cool}+${this.champion.characterBonus.cool}`;
+
+    return String(this.champion.profileWithBonuses().cool);
+  }
+
+  willpowerStatDisplay(): string {
+    if (this.champion?.characterBonus == null) return '';
+
+    if (this.champion.characterBonus.willPower > 0)
+      return `${this.champion.profileWithBonuses().willPower}+${this.champion.characterBonus.willPower}`;
+
+    return String(this.champion.profileWithBonuses().willPower);
+  }
 }
